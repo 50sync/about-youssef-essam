744f8f6163f38ad86b5fd2523878cf135efd1581
50sync <youseefessamh@gmail.com>
2026-01-06 16:10:36 +0200

    Deployment issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router/Router";
-import "./Styles/Styles.css";
-import "./Styles/Variables.css";
 
 export default function App() {
   return <RouterProvider router={router} />;
